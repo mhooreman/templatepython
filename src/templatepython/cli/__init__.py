4e9e9009079908de1161faf993e3a5d0dffc4330
Michaël Hooreman <mhooreman@icloud.com>
2024-10-15 10:04:22 +0200

    Moved logging to loguru

--- src/templatepython/cli/__init__.py
+++ src/templatepython/cli/__init__.py
@@ -3,6 +3,7 @@
 """Command line interface."""
 
 import click
+from loguru import logger
 
 from templatepython.__about__ import __version__
 
@@ -16,4 +17,6 @@ from templatepython.__about__ import __version__
 )
 def templatepython() -> None:
     """Do the same thing as any template project: say hello."""
+    logger.info("Starting")
     click.echo("Hello world!")
+    logger.info("Completed")
